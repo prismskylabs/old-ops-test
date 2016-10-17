af677b40576eedc7f5eae803ca325c319576c5a2
Chris Ralston <cralston0@users.noreply.github.com>
2016-10-17 10:46:08 -0700

    new feature: factorial function in node source

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
 'use strict';
 
+function factorial(n) {
+    if (n < 0) {
+        throw "Can't calculate factorial of negative number";
+    }
+    if (n <= 1) {
+        return n;
+    }
+    return (n * factorial(n-1));
+}
+
 (function main() {
     console.log('Hello, world!');
 })();
