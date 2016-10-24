2af491349e75d4910479713ca009a676db38ef5a
Chris Ralston <cralston0@users.noreply.github.com>
2016-10-24 14:42:32 -0700

    new feature: add factorial to C src to match other branches

--- src/main.c
+++ src/main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+int factorial(n) {
+    if (n < 0) {
+        return -1; /* don't do that! */
+    }
+    if (n <= 1) {
+        return n;
+    }
+    return n * factorial(n-1);
+}
+
 int main(int argc, char *argv[]) {
     printf("Hello, world!\n");
     return 0;
